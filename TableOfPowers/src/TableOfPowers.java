7245500243f5083bf24bb706a66179bf1651bbc1
jimpatton <jimpatton5150@gmail.com>
2025-03-05 14:59:15 -0500

    init commit

---
+++ TableOfPowers/src/TableOfPowers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TableOfPowers {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Squares and Cubes table\n");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			
+			System.out.print("Enter an integer ");
+			int nbr = sc.nextInt();
+			int nbrSq = nbr*nbr;
+			int nbrCb = nbr*nbr*nbr;
+			System.out.println("Number\t\tSquared\t\tCubed");
+			System.out.println("======\t\t=======\t\t=====");
+			for (int i = i = 1; i<=nbr; i++) {
+				System.out.println(i +"\t\t" + i*i + "\t\t" + i*i*i);
+			}
+			
+			sc.nextLine();
+			System.out.print("\nContinue? (y/n): ");
+			choice = sc.nextLine();
+			
+		}
+				System.out.println("\nBye");
+
+	}
+
+}
