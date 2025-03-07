43e76b45d19771b662dcc6f2e2cf20fd89ca4275
jimpatton <jimpatton5150@gmail.com>
2025-03-07 15:23:03 -0500

    init commit

---
+++ MakeChocolate/src/MakeChocolate.java
@@ -0,0 +1,35 @@
+import util.MyConsole;
+
+public class MakeChocolate {
+
+	public static void main(String[] args) {
+		MyConsole.printHeader("Welcome to Make Chocolate");
+		// given goal(kilos), #of big(5 kilos), #of small(1 kilo)
+		// determine how many smallBars to use to hit the goal
+		// goal - (big*5) = smallBars
+		// if smallBars <= small
+		// return small
+		// else
+		// return -1
+
+		int smallBarsUsed = makeChocolate(4,1,9);
+		MyConsole.printLine("small bars used = " +smallBarsUsed);
+		
+		smallBarsUsed = makeChocolate(4,1,10);
+		MyConsole.printLine("small bars used = " +smallBarsUsed);
+		
+		smallBarsUsed = makeChocolate(4,1,7);
+		MyConsole.printLine("small bars used = " +smallBarsUsed);
+		
+	}
+
+	public static int makeChocolate(int small, int big, int goal) {
+		int smallBars = goal - (big * 5);
+		if (smallBars <= small) {
+			MyConsole.printLine("The number of small bars needed for goal is: " + smallBars);
+		} else {
+			smallBars = -1;
+		}
+		return smallBars;
+	}
+}
