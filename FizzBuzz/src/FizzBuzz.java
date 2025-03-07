563900b370fdf3dc2101b226ec99cc488d1affd2
jimpatton <jimpatton5150@gmail.com>
2025-03-06 22:24:05 -0500

    init commit

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -5,12 +5,15 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		System.out.println("FizzBuzz\n");
 		
-		Scanner sc = new Scanner(System.in);		
+		Scanner sc = new Scanner(System.in);	
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
 		System.out.print("Enter a number:");
 		int nbr = sc.nextInt();
+		
 		for (int i = 1; i<=nbr; i++) {
 					
-		if (i % 5==0 && i % 3==0) {
+		if (i % 15==0 ) {
 			System.out.println("fizzbuzz");
 		}
 		else if (i % 3 == 0) {
@@ -23,8 +26,10 @@ public class FizzBuzz {
 			System.out.println(i);
 			
 		}
-		
-		
+		sc.nextLine();
+		System.out.println("Continue? ");
+		choice = sc.nextLine();
+		}
 		System.out.println("\nBye");
 
 	}
