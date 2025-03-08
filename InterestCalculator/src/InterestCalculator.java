892b3aab66d1c0ba6932ae204ace22c243634150
jimpatton <jimpatton5150@gmail.com>
2025-03-08 14:04:47 -0500

    updated

--- InterestCalculator/src/InterestCalculator.java
+++ InterestCalculator/src/InterestCalculator.java
@@ -6,33 +6,33 @@ public class InterestCalculator {
 
 	public static void main(String[] args) {
 		MyConsole.printHeader("Welcome to the Interest Calculator");
-		//while loop for Continue
+		// while loop for Continue
 		String choice = "y";
-		while (choice.equalsIgnoreCase("y")) {		
-		// prompt for loan amount: double
-		double loan =MyConsole.promptDouble("Enter loan amount:\t ");
-		NumberFormat loanAmount = NumberFormat.getCurrencyInstance();
-		
-		
-		//prompt for interest rate: double
-		double rate = MyConsole.promptDouble("Enter interest rate:\t ");
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		percent.setMaximumFractionDigits(3);
-		
-		//calculate interest: loan * rate
-		double interest = loan * rate;
-		NumberFormat interestAmount = NumberFormat.getCurrencyInstance();
+		while (choice.equalsIgnoreCase("y")) {
+			// prompt for loan amount: double
+			double loan = MyConsole.promptDouble("Enter loan amount:\t ");
+			NumberFormat loanAmount = NumberFormat.getCurrencyInstance();
 
-		//print loan amount in currency
-		MyConsole.printLine("\nLoan amount:\t\t"+loanAmount.format(loan));;
-		//print interest rate in percent to 3 decimal places
-		MyConsole.printLine("Interest rate:\t\t"+percent.format(rate));
-		//print interest in currency
-		MyConsole.printLine("Interest:\t\t"+interestAmount.format(interest));
+			// prompt for interest rate: double
+			double rate = MyConsole.promptDouble("Enter interest rate:\t ");
+			NumberFormat interestRate = NumberFormat.getPercentInstance();
+			interestRate.setMaximumFractionDigits(3);
 
-		choice = MyConsole.promptString("Continue? (y/n): ");
+			// calculate interest: loan * rate
+			double interest = loan * rate;
+			NumberFormat interestAmount = NumberFormat.getCurrencyInstance();
+
+			// print loan amount in currency
+			MyConsole.printLine("\nLoan amount:\t\t" + loanAmount.format(loan));
+			;
+			// print interest rate in percent to 3 decimal places
+			MyConsole.printLine("Interest rate:\t\t" + interestRate.format(rate));
+			// print interest in currency
+			MyConsole.printLine("Interest:\t\t" + interestAmount.format(interest));
+
+			choice = MyConsole.promptString("Continue? (y/n): ");
+		}
+		MyConsole.printLine("\nBye");
+
 	}
-	 MyConsole.printLine("\nBye");
-
 }
-	}
