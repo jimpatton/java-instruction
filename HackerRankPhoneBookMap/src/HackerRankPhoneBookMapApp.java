a61600c3659a8bb93d863ba5f21cff3e37d831dc
jimpatton <jimpatton5150@gmail.com>
2025-03-19 18:22:40 -0400

    init commit

---
+++ HackerRankPhoneBookMap/src/HackerRankPhoneBookMapApp.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HackerRankPhoneBookMapApp {
+
+	public static void main(String[] args) {
+				
+		String inputStr = ("3\nuncle sam\n99912222\ntom\n11122222\nharry\n12299933\nuncle sam\nuncle tom\nharry");
+		Scanner in = new Scanner(inputStr);
+		int n=in.nextInt();
+		in.nextLine();
+		
+		Map <String, Integer> phonebook = new HashMap<String, Integer>();
+		
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+			in.nextLine();
+			phonebook.put(name, phone);
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+			if (phonebook.containsKey(s)){
+				System.out.println(s+ " = " + phonebook.get(s));
+				}
+			else {
+				System.out.println("Not Found");
+			}
+		}
+		
+	}
+
+}
