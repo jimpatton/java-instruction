26fb0267301f754add2960435fb82b06f3683e36
jimpatton <jimpatton5150@gmail.com>
2025-03-23 12:15:00 -0400

    init commit

---
+++ HackerRankJavaIfElse/src/HackerRankJaveIfElseApp.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class HackerRankJaveIfElseApp {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		 
+		int nbr = scan.nextInt();
+	        scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	      
+	        
+	        for (int i = 1; i<=nbr; i++) {
+	        	if (nbr % 2 != 0) {
+	        		System.out.println("Weird");	        		
+	        	}
+	        	else if(nbr % 2== 0 && nbr < 5) {
+	        		System.out.println("Not Weird");
+	        	}
+	        	else if (nbr % 2 ==0 && nbr<= 20) {
+	        		System.out.println("Weird");
+	        	}
+	        	else if(nbr % 2 == 0 && nbr >20) {
+	        		System.out.println("Not Weird");
+	        	}
+	        	scan.next();
+	        	
+	        }
+
+	        scan.close();
+
+	}
+
+}
