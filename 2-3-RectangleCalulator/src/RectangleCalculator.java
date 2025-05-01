619f19b54614fc1ff5040262c2e894c88ce82387
jimpatton <jimpatton5150@gmail.com>
2025-05-01 18:45:27 -0400

    rectangle validation

--- 2-3-RectangleCalulator/src/RectangleCalculator.java
+++ 2-3-RectangleCalulator/src/RectangleCalculator.java
@@ -7,15 +7,15 @@ public class RectangleCalculator {
 		System.out.println("Welcome to the Rectangle Area and Perimeter Calculator");
 		String choice = "y";
 		while(choice.equalsIgnoreCase("y")) {
-			System.out.print("\nEnter length: ");
-			int l = sc.nextInt();
-			System.out.print("Enter width: ");
-			int w = sc.nextInt();
-			sc.nextLine();
+//			System.out.print("\nEnter length: ");
+			double l = getPositiveDouble(sc,"\nEnter length: ");
+//			System.out.print("Enter width: ");
+			double w = getPositiveDouble(sc,"Enter width: ");
+//			sc.nextLine();
 			//area = l*w
 			//perimeter 2*(l+w)
-			System.out.println("Area: " + (l*w));
-			System.out.println("Perimeter: " + 2*(l+w));
+			System.out.printf("Area: %.2f%n", (l*w));
+			System.out.printf("Perimeter: %.2f%n", 2*(l+w));
 			System.out.print("\nContinue ? (y/n): ");
 			choice = sc.nextLine();
 				
@@ -23,5 +23,26 @@ public class RectangleCalculator {
 		sc.close();
 
 	}
-
+	private static double getPositiveDouble(Scanner sc, String prompt) {
+		double num = -1;
+		while (true) {
+			System.out.print(prompt);
+			String input = sc.nextLine();
+			try {
+				num = Double.parseDouble(input);
+				if (num >0) {
+					break;
+				}
+				else {
+					System.out.println("Error! Please enter a positive number.");
+				}
+			}
+			catch (NumberFormatException e){
+				System.out.println("Error! Please enter a numeric value.");
+				
+			}		
+		}
+		return num;
+	}
+	
 }
